919ade2c6d76ab01eb7a0a2e4f83212d8ee33577
Microscopy <84872500+BiologyTools@users.noreply.github.com>
2022-09-11 04:27:08 +1300

    Updated for Bio 2.3.1

--- Points.cs
+++ Points.cs
@@ -1,9 +1,11 @@
-//css_reference BioImage.dll;
+//css_reference Bio.dll;
+
 using System;
 using System.Windows.Forms;
 using System.Drawing;
-using BioImage;
+using Bio;
 using System.Threading;
+
 public class Loader
 {
 	//Point ROI Tool Example
@@ -17,18 +19,20 @@ public class Loader
 			{
 				if (!s.processed)
 				{
-					if (s.type == Bio.Scripting.Event.Up && s.buts == MouseButtons.Left)
+					if (s.type == Bio.Scripting.Event.Down && s.buts == MouseButtons.Left)
 					{
-						ZCT cord = ImageView.viewer.GetCoordinate();
-						Annotation an = Annotation.CreatePoint(cord, s.p.X, s.p.Y);
-						ImageView.viewer.image.Annotations.Add(an);
+						ZCT cord = Bio.App.viewer.GetCoordinate();
+						Bio.Scripting.LogLine(cord.ToString() + " Coordinate");
+						Bio.ROI an = Bio.ROI.CreatePoint(cord, s.p.X, s.p.Y);
+						Bio.ImageView.SelectedImage.Annotations.Add(an);
+						Bio.Scripting.LogLine(cord.ToString() + " Coordinate");
 						an.Text = "Point" + ind;
 						ind++;
-						Bio.Scripting.LogLine(s.ToString());
-						ImageView.viewer.UpdateOverlay();
+						Bio.Scripting.LogLine(s.ToString() + " Point");
+						//ImageView.viewer.UpdateOverlay();
 					}
 					else
-					if (s.type == Bio.Scripting.Event.Down)
+					if (s.type == Bio.Scripting.Event.Up)
 					{
 						Bio.Scripting.LogLine(s.ToString());
 					}
@@ -37,12 +41,10 @@ public class Loader
 					{
 						Bio.Scripting.LogLine(s.ToString());
 					}
-				}
-				{
 					s.processed = true;
 				}
 			}
 		} while (true);
-		return "Done";
+		return "OK";
 	}
 }
